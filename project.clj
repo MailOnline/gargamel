aaa027ff78f8faef22342287fc6ed034875b36bc
Benedek Fazekas <benedek.fazekas@mailonline.co.uk>
2014-11-08 19:03:25 +0000

    bumps up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gargamel "0.1.0"
+(defproject gargamel "0.2.0-SNAPSHOT"
   :description "Generates changelog between two git refs."
   :url "https://github.com/MailOnline/gargamel"
   :license {:name "Eclipse Public License"
