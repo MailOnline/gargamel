5b73191637e55b007cb6dec571891f2995186453
Benedek Fazekas <benedek.fazekas@mailonline.co.uk>
2015-01-06 11:36:12 +0000

    excludes clojure as dep

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/MailOnline/gargamel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[stencil "0.3.2"]
-                 [org.clojure/tools.cli "0.3.1"]
-                 [cheshire "5.3.1"]]
+  :dependencies [[stencil "0.3.2" :exclusions [org.clojure/clojure]]
+                 [org.clojure/tools.cli "0.3.1":exclusions [org.clojure/clojure]]
+                 [cheshire "5.3.1":exclusions [org.clojure/clojure]]]
   :profiles {:uberjar {:main gargamel.core
                        :aot :all}
              :test {:resource-paths ["test-resources"]}})
