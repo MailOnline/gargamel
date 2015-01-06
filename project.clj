7566f41ddba314c2ee4e1389497166a9c3ab7869
Benedek Fazekas <benedek.fazekas@mailonline.co.uk>
2015-01-06 11:44:01 +0000

    explicit dep on clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/MailOnline/gargamel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[stencil "0.3.2" :exclusions [org.clojure/clojure]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [stencil "0.3.2" :exclusions [org.clojure/clojure]]
                  [org.clojure/tools.cli "0.3.1":exclusions [org.clojure/clojure]]
                  [cheshire "5.3.1":exclusions [org.clojure/clojure]]]
   :profiles {:uberjar {:main gargamel.core
