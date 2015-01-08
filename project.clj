b5cdbc3d88dc79cd64bc9e82a471413c87f8ded2
Benedek Fazekas <benedek.fazekas@mailonline.co.uk>
2015-01-08 10:21:05 +0000

    0.5.0alpha1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gargamel "0.5.0-SNAPSHOT"
+(defproject gargamel "0.5.0-alpha1"
   :description "Generates changelog between two git refs."
   :url "https://github.com/MailOnline/gargamel"
   :license {:name "Eclipse Public License"
